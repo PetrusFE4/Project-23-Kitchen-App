6c1d6b3e198fa36697b38f50b563e8e67ba99076
Vodkaaa <rejkacandra231@gmail.com>
2024-06-02 20:05:39 +0700

    Setup
    setup project ResepIn Aja

---
+++ ResepIn_Aja/src/App.jsx
@@ -0,0 +1,10 @@
+function App() {
+
+  return (
+    <>
+      <div className="App">HEllo world</div>
+    </>
+  )
+}
+
+export default App
---
+++ ResepIn_Aja/src/components/Login.jsx
@@ -0,0 +1,10 @@
+
+const Login = () => {
+  return (
+    <div>
+      Ini Login
+    </div>
+  )
+}
+
+export default Login
