4c1da2fc78165502be801abaf5e309899b43cd5e
Fawwaz Yasirah <fawwazyasirah@gmail.com>
2024-06-10 11:57:13 +0700

    auth progress

--- api/controlllers/userController.js
+++ api/controlllers/userController.js
@@ -1,24 +1,13 @@
 const models = require('../models');
+const bycrptjs = require('bcryptjs');
+const jwt = require('jsonwebtoken');
 
 function register(req,res){
-    const user = {
-        username: req.body.username,
-        email: req.body.email,
-        password: req.body.password
-    }
+    models.user.findOne({email: req.body.email})
+    
 
-    models.user.create(user).then(result=>{
-
-        res.status(201).json({
-            message: "user registered successfuly",
-            resep: result
-        });
-    }).catch(error=>{
-        res.status(500).json({
-            message: "Something wrong",
-            error: error
-        });
-    });
+    
+    
 }
 
 
