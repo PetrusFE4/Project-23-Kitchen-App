7a8256d657b66f4b67621066bd5f7efb02a27cb4
Ariwibowogithub <arie35411@gmai.com>
2024-06-10 12:12:52 +0700

    update aboutUs

--- ResepIn_Aja/src/components/AboutUs.jsx
+++ ResepIn_Aja/src/components/AboutUs.jsx
@@ -1,5 +1,105 @@
 const AboutUs = () => {
-  return <div className="about-section"></div>;
+  return (
+    <>
+      <div className="about-section">
+        <h1>About Us Page</h1>
+        <h4 className="sub-title text-warning">
+          Kami adalah Team Developer ResepIn_Aja
+        </h4>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorem
+          consequuntur amet magni a dicta repudiandae consectetur laborum
+          necessitatibus provident. Maiores voluptatem culpa aliquid quis
+          inventore laudantium magnam, ipsam at ab.
+        </p>
+      </div>
+      <h2 className="text-team text-center"> THIS IS OUR TEAM ....</h2>
+      <div className="container">
+        <div className="card" style={{ width: "18rem" }}>
+          <img
+            src="/src/assets/img/team3.jpg"
+            className="card-img-top"
+            alt="..."
+          />
+          <div className="card-body">
+            <h5 className="card-title">ARI Wibowo</h5>
+            <p className="card-text">
+              Some quick example text to build on the card title and make up
+            </p>
+            <a href="#" className="btn btn-light text-black">
+              Get in touch with me
+            </a>
+          </div>
+        </div>
+        <div className="card" style={{ width: "18rem" }}>
+          <img
+            src="/src/assets/img/team3.jpg"
+            className="card-img-top"
+            alt="..."
+          />
+          <div className="card-body">
+            <h5 className="card-title">Rejka Aditiya</h5>
+            <p className="card-text">
+              Some quick example text to build on the card title and make up
+            </p>
+            <a href="#" className="btn btn-light text-black">
+              Get in touch with me
+            </a>
+          </div>
+        </div>
+        <div className="card" style={{ width: "18rem" }}>
+          <img
+            src="/src/assets/img/team3.jpg"
+            className="card-img-top"
+            alt="..."
+          />
+          <div className="card-body">
+            <h5 className="card-title">Rendy Rizky</h5>
+            <p className="card-text">
+              Some quick example text to build on the card title and make up
+            </p>
+            <a href="#" className="btn btn-light text-black">
+              Get in touch with me
+            </a>
+          </div>
+        </div>
+      </div>
+      <div className="container">
+        <div className="card" style={{ width: "18rem" }}>
+          <img
+            src="/src/assets/img/team3.jpg"
+            className="card-img-top"
+            alt="..."
+          />
+          <div className="card-body">
+            <h5 className="card-title">Fawas</h5>
+            <p className="card-text">
+              Some quick example text to build on the card title and make up
+            </p>
+            <a href="#" className="btn btn-light text-black">
+              Get in touch with me
+            </a>
+          </div>
+        </div>
+        <div className="card" style={{ width: "18rem" }}>
+          <img
+            src="/src/assets/img/team3.jpg"
+            className="card-img-top"
+            alt="..."
+          />
+          <div className="card-body">
+            <h5 className="card-title">Bagus Aji</h5>
+            <p className="card-text">
+              Some quick example text to build on the card title and make up
+            </p>
+            <a href="#" className="btn btn-light text-black">
+              Get in touch with me
+            </a>
+          </div>
+        </div>
+      </div>
+    </>
+  );
 };
 
 export default AboutUs;
