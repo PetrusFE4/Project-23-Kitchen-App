e45ac01e9e8404b042082d2cc0c3676c8cf167c8
Fawwaz Yasirah <fawwazyasirah@gmail.com>
2024-06-12 17:36:52 +0700

    image uploader fix

--- backend/controlllers/imageController.js
+++ backend/controlllers/imageController.js
@@ -1,24 +1,24 @@
 // SCRIPT DARI YOUTUBE
-// function uploadImage(req, res) {
-//     if(req.file.filename){
-//         res.status(201).json({
-//             message: "Gambar berhasil di unggah!",
-//             url: req.file.filename
-//         });
-//     }else{
-//         res.status(500).json({
-//             message: "Terjadi kesalahan!"
-//         });
-//     }
-// }
+ function uploadImage(req, res) {
+    if(req.file.filename){
+        res.status(201).json({
+            message: "Gambar berhasil di unggah!",
+            url: req.file.filename
+         });
+     }else{
+         res.status(500).json({
+             message: "Terjadi kesalahan!"
+         });
+    }
+ }
 
-// module.exports = {
-//     uploadImage: uploadImage
-// }
+ module.exports = {
+     uploadImage: uploadImage
+ }
 
 
 // SCRIPT DARI GPT
-exports.uploadImage = (req, res, next) => {
+/* exports.uploadImage = (req, res, next) => {
     if (!req.file) {
         return res.status(400).json({ message: 'No file uploaded' });
     }
@@ -29,4 +29,4 @@ exports.uploadImage = (req, res, next) => {
         message: 'File uploaded successfully',
         filename: filename
     });
-};
+}; */
