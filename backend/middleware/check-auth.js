53734413ead98cd3a7aaeb3339cc61c6256da414
Fawwaz Yasirah <103474945+pootism69@users.noreply.github.com>
2024-06-15 06:42:55 +0700

    Update check-auth.js

--- backend/middleware/check-auth.js
+++ backend/middleware/check-auth.js
@@ -1,9 +1,12 @@
 const jwt = require('jsonwebtoken');
+const dotenv = require('dotenv')
+
+dotenv.config()
 
 function checkAuth(req, res, next) {
     try{
         const token = req.headers.authorization.split(" ")[1];
-        const decodedToken = jwt.verify(token, process.env.JWT_KEY);
+        const decodedToken = jwt.verify(token, process.env.secret);
         req.userData = decodedToken;
         next();
     }catch(e) {
