f7a86b01859c9eef53741306906985242279723a
Ariwibowogithub <arie35411@gmai.com>
2024-06-16 20:24:08 +0700

    update form

--- backend/apps.js
+++ backend/apps.js
@@ -11,7 +11,7 @@
 //         credentials: true,
 //         allowedHeaders: ["Content-Type", "Authorization", "Cookies"],
 //       })
-    
+
 // )
 // app.use(express.urlencoded({ extended: true }));
 // const route = require('./routes/web');
@@ -23,11 +23,11 @@
 // // app.use('/user', userRoute);
 // module.exports = app;
 
-const express = require('express');
-const bodyParser = require('body-parser');
-const cors = require('cors');
-const Sequelize = require('sequelize');
-const dotenv = require('dotenv');
+const express = require("express");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+const Sequelize = require("sequelize");
+const dotenv = require("dotenv");
 
 const app = express();
 
@@ -35,33 +35,37 @@ const app = express();
 dotenv.config();
 
 // Middleware CORS
-app.use(cors({
-  origin: ["http://localhost:5173"],
-  credentials: true,
-  allowedHeaders: ["Content-Type", "Authorization", "Cookies"],
-}));
+app.use(
+  cors({
+    origin: ["http://localhost:5173"],
+    credentials: true,
+    allowedHeaders: ["Content-Type", "Authorization", "Cookies"],
+  })
+);
 
 // Middleware Body Parser
 app.use(bodyParser.json());
 
 // Routes
-app.get("/", (req, res) => {
-  res.send("Selamat datang di kitchen app");
-});
+// app.get("/", (req, res) => {
+//   res.send("Selamat datang di kitchen app");
+// });
 
 // Menggunakan routing dari file web.js di bawah /cek
-const route = require('./routes/web');
-app.use('/cek', route);
+const route = require("./routes/web");
+app.use("/", route);
 
 // Mengakses folder uploads secara statis
-app.use('/uploads', express.static('uploads'));
+app.use("/uploads", express.static("uploads"));
 
 // Load environment configuration
-const environment = process.env.NODE_ENV || 'development';
+const environment = process.env.NODE_ENV || "development";
 const config = require(`./config/config.json`)[environment];
 
-const sequelize = new Sequelize(config.DATABASE_URL || {
-  dialect: 'mysql'
-});
+const sequelize = new Sequelize(
+  config.DATABASE_URL || {
+    dialect: "mysql",
+  }
+);
 
 module.exports = app;
